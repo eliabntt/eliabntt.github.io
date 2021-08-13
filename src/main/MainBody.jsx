4550daf71c76d74c9fc09e5fcf7628b72002ef70
Elia Bonetto <eliabntt94@gmail.com>
2021-08-13 08:21:51 +0200

    slideshow

--- src/main/MainBody.jsx
+++ src/main/MainBody.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Typist from "react-typist";
+import { NavHashLink as Link } from 'react-router-hash-link';
 import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
 
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+const scrollWithOffset = (el) => {
+  const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
+  const yOffset = -40; 
+  window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' }); 
+}
+
 const MainBody = React.forwardRef(
-  ({ gradient, title, message, icons }, ref) => {
+  ({ gradient, title, message, icons, imgs}, ref) => {
     return (
+      
       <Jumbotron
         fluid
         id="home"
@@ -13,11 +24,18 @@ const MainBody = React.forwardRef(
           background: `linear-gradient(136deg,${gradient})`,
           backgroundSize: "1200% 1200%",
         }}
-        className="title bg-transparent bgstyle text-light min-vh-100 d-flex align-content-center align-items-center flex-wrap m-0"
-      >
-        <div id="stars"></div>
-        <Container className="text-center">
-          <h1 ref={ref} className="display-1">
+        className="title bg-transparent bgstyle text-light min-vh-100 d-flex align-content-center align-items-center flex-wrap m-0"> 
+        
+        <Carousel showArrows={false} autoPlay={true} transitionTime={2000} interval={10000} dynamicHeight={false} infiniteLoop={true} showThumbs={false} showStatus={false} centerMode={false}>
+                {imgs.map((img, index) => (
+                  <div key={`img-${index}`}>
+                      <img src={img.img}/>
+                  </div>
+                ))}
+        </Carousel>
+        <div id="stars" className="overlay-nc"></div>
+        <Container className="overlay text-center">
+          <h1 className="display-1">
             {title}
           </h1>
           <Typist className="lead typist" cursor={{ show: false }}>
@@ -37,16 +55,13 @@ const MainBody = React.forwardRef(
               </a>
             ))}
           </div>
-          <a
-            className="btn btn-outline-light btn-lg "
-            href="#aboutme"
-            role="button"
-            aria-label="Learn more about me"
-          >
+          
+          <Link ref={ref} className="btn btn-outline-light btn-lg" to={`/#aboutme`} scroll={el => scrollWithOffset(el)} role="button">          
             More about me
-          </a>
-        </Container>
+          </Link>
+          </Container>
       </Jumbotron>
+
     );
   }
 );
